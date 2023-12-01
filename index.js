ae7cc87ee72dc2d18c14ba7fc8effdadbe02dc35
Ararat0Grigoryan <arogrigoryan0203@gmail.com>
2023-12-01 16:23:04 +0400

    media add (670px)(599px)

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ document.addEventListener('click', function (event) {
 
 function toggleMenu() {
     var mobileMenu = document.querySelector('.mobile-menu');
-    mobileMenu.style.display === 'block' ? mobileMenu.style.display = 'none' : mobileMenu.style.display = 'block';
+    mobileMenu.style.display === '' ? mobileMenu.style.display = 'none' : mobileMenu.style.display = '';
 }
