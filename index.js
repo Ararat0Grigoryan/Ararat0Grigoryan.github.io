33830df4968a93cbf2a6c4d55810c6945e70dc3f
Ararat0Grigoryan <arogrigoryan0203@gmail.com>
2023-12-02 13:53:40 +0400

    add logo mobile

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ document.addEventListener('click', function (event) {
 });
 
 
+
+// Media Menu
 function toggleMenu() {
     var mobileMenu = document.querySelector('.mobile-menu');
     mobileMenu.style.display === '' ? mobileMenu.style.display = 'none' : mobileMenu.style.display = '';
 }
+// 
